8fc980b085cdb22663b57838d8ed0ed18dbe8f02
Drake_Mapel <40637038+dmapel@users.noreply.github.com>
2018-08-29 15:45:33 -0400

    gitignore 2

--- 1806-June25/src/main/java/com/ex/model/web/BookServlet.java
+++ 1806-June25/src/main/java/com/ex/model/web/BookServlet.java
@@ -7,20 +7,40 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
 import java.io.IOException;
 
 public class BookServlet extends HttpServlet{
+	/*Logging levels
+	 	TRACE
+	 	DEBUG
+	 	INFO
+	 	WARN
+	 	ERROR
+	 	FATAL
+	 	ALL
+	 */
+	static Logger logger = Logger.getLogger(BookServlet.class);
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // harcoding since before it was cool
+        // hardcoding since before it was cool
+    	logger.info("Received request for book");
         Book b = new Book();
         b.setTitle("Web Programming for Dummies");
         b.setAuthor("August Duet");
         b.setPubYear("2020");
+        
+        logger.info(String.format("Sending response %s", b.getTitle()));
 
         String bJson = new ObjectMapper().writeValueAsString(b);
         resp.getWriter().write(bJson);
         resp.setStatus(200);
         resp.setHeader("Content-Type", "application/json");
+        
+        System.out.println("Response Sent");
+        logger.trace("Response Sent");
+        logger.info("Response Sent");
     }
 }
